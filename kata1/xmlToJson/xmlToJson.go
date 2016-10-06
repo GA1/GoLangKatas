402263896207dcefbc7082532a97f5ed6037a278
Kamil Nguyen Van <knguyen@zinio.com>
2016-10-06 17:31:27 +0200

    Add go channels

--- kata1/xmlToJson/xmlToJson.go
+++ kata1/xmlToJson/xmlToJson.go
@@ -15,6 +15,28 @@ type Stock struct {
     } `xml:"Product" json:"products"`
 }
 
+type Parser struct {
+    close chan string
+}
+
+func (p Parser) xmlToJson(xmlData []byte) {
+
+    time.Sleep(time.Duration(rand.Int31n(1999)) * time.Millisecond)
+    var stock Stock
+    errXml := xml.Unmarshal(xmlData, &stock)
+    if errXml != nil {
+        return
+    }
+
+    stockJson, errJson := json.Marshal(stock)
+
+    if errJson != nil {
+        return
+    }
+
+    p.close <- string(stockJson)
+}
+
 func main() {
 
     xmlData := []byte(`<?xml version="1.0" encoding="UTF-8" ?>
@@ -29,28 +51,17 @@ func main() {
         </Product>
     </ProductList>`)
 
+    done := make(chan string)
+
+    var parser Parser
+
     for i := 0; i < 10; i++ {
-        fmt.Println(xmlToJson(xmlData))
+        parser = Parser{done}
+        go parser.xmlToJson(xmlData)
     }
 
+    supu := <-done
+    fmt.Println(supu)
+    fmt.Println("The end")
 }
-
-func xmlToJson(xmlData []byte) (string, error) {
-
-    time.Sleep(time.Duration(rand.Int31n(1999)) * time.Millisecond)
-    var stock Stock
-    errXml := xml.Unmarshal(xmlData, &stock)
-    if errXml != nil {
-        return "", errXml
-    }
-
-    stockJson, errJson := json.Marshal(stock)
-
-    if errJson != nil {
-        return "", errJson
-    }
-
-    return string(stockJson), nil
-}
-
 
