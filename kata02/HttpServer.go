a018716e75c3fd8204f01a8cccc915d6d78c9ef3
Kamil Nguyen Van <knguyen@zinio.com>
2016-10-13 15:27:30 +0200

    Add flag read from commandLine

--- kata02/HttpServer.go
+++ kata02/HttpServer.go
@@ -1,13 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+    "github.com/gin-gonic/gin"
+    "fmt"
+    "flag"
+)
 
 func main() {
+    var port = flag.Int("port", 1234, "help message for flagname")
+    flag.Parse()
+    fmt.Println(*port)
+
     r := gin.Default()
     r.GET("/ping", func(c *gin.Context) {
         c.JSON(200, gin.H{
             "message": "pong",
         })
     })
-    r.Run() // listen and server on 0.0.0.0:8080
+    r.Run("0.0.0.0:8081") // listen and server on 0.0.0.0:8080
 }
